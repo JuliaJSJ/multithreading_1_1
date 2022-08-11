f1e933de24d57393722d77ba88c45e862913c3ad
JuliaJSJ <80544651+JuliaJSJ@users.noreply.github.com>
2022-08-11 10:32:35 -0400

    Create MyThread.java

---
+++ src/main/java/MyThread.java
@@ -0,0 +1,19 @@
+class MyThread extends Thread {
+
+    public MyThread(ThreadGroup group, String name){
+        super(group, name);
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!isInterrupted()) {
+                Thread.sleep(2500);
+                System.out.println("Всем привет!" + " Я поток " + getName());
+            }
+        } catch (InterruptedException err) {
+
+        }
+    }
+
+}
